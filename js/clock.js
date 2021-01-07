319ecce45a3fd3388940b88f359ddd5931144f90
kkovacsics <kalman.kovacsics@gmail.com>
2021-01-07 16:36:31 +0100

    ESLint használat

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,11 @@
-'use strict';
-
 const clock = document.querySelector('.clock');
 
 // setInterval( () => {
-//     const now = new Date();
-//     clock.textContent= `${now.getHours()}:${String(now.getMinutes()).padStart(2,0)}:${String(now.getSeconds()).padStart(2,0)}`;
+//   const now = new Date();
+// eslint-disable-next-line max-len
+//   clock.textContent = `${now.getHours()}:${String(now.getMinutes()).padStart(2,0)}:${String(now.getSeconds()).padStart(2,0)}`;
 // }, 1000);
 
-setInterval( () => {
-    clock.textContent= new Date().toLocaleTimeString();
+setInterval(() => {
+  clock.textContent = new Date().toLocaleTimeString();
 }, 1000);
